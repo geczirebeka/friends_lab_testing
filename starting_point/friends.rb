2d8390ac1e80d89af38aad0773a12cb21347173a
Helena <inger.helena@gmail.com>
2020-06-18 14:28:58 +0100

    test total money pass

--- starting_point/friends.rb
+++ starting_point/friends.rb
@@ -17,3 +17,11 @@ end
 def remove_friend(person, friend)
   person[:friends].delete(friend)
 end
+
+def total_money(people)
+  total = 0
+  for person in people
+    total += person[:monies]
+  end
+  return total
+end
--- starting_point/specs/friends_spec.rb
+++ starting_point/specs/friends_spec.rb
@@ -109,6 +109,10 @@ end
 
   # 6. Find the total of everyone's money
   # (hint: use the @people array, remember how we checked the total number of eggs yesterday?)
+  def test_total_money
+    result = total_money(@people)
+    assert_equal(143, result)
+  end
 
 
   # 7. For two given people, allow the first person to loan a given value of money to the other
